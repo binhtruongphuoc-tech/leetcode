9ea7eb9a2ea161e984cb40693004becc9338ad2c
Binh Truong Phuoc <binh.truongphuoc2501@hcmut.edu.vn>
2026-06-15 10:08:33 +0700

    Time: 0 ms (100%), Space: 12 MB (28.76%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
@@ -0,0 +1,39 @@
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    if (nums1Size > nums2Size) {
+        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
+    }
+
+    int x = nums1Size;
+    int y = nums2Size;
+    int low = 0;
+    int high = x;
+
+    while (low <= high) {
+        int partitionX = (low + high) / 2;
+        int partitionY = (x + y + 1) / 2 - partitionX;
+        int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
+        int minRightX = (partitionX == x) ? INT_MAX : nums1[partitionX];
+
+        int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
+        int minRightY = (partitionY == y) ? INT_MAX : nums2[partitionY];
+        if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+            if ((x + y) % 2 == 0) {
+                return ((double)MAX(maxLeftX, maxLeftY) + MIN(minRightX, minRightY)) / 2.0;
+            } 
+            else {
+                return (double)MAX(maxLeftX, maxLeftY);
+            }
+        }
+        else if (maxLeftX > minRightY) {
+            high = partitionX - 1;
+        }
+        else {
+            low = partitionX + 1;
+        }
+    }
+
+    return 0.0; 
+}
