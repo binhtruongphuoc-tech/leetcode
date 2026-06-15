ec53e1a88bcd7ba2aa2f370140e07cd44c7bbe53
Binh Truong Phuoc <binh.truongphuoc2501@hcmut.edu.vn>
2026-06-15 10:52:04 +0700

    Time: 0 ms (100%), Space: 9.9 MB (92.21%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.c
@@ -0,0 +1,25 @@
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i]++;
+            *returnSize = digitsSize;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    *returnSize = digitsSize + 1;
+    int* newDigits = (int*)malloc((*returnSize) * sizeof(int));
+    newDigits[0] = 1;
+    for (int i = 1; i < *returnSize; i++) {
+        newDigits[i] = 0;
+    }  
+    return newDigits;
+}
+void printArray(int* arr, int size) {
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("%d", arr[i]);
+        if (i < size - 1) printf(",");
+    }
+    printf("]\n");
+}
