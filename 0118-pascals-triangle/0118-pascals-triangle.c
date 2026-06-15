93c4a82d8651cb42792c684841ee5d47b9f8f7ec
Binh Truong Phuoc <binh.truongphuoc2501@hcmut.edu.vn>
2026-06-15 10:58:39 +0700

    Time: 0 ms (100%), Space: 11.6 MB (31.44%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.c
@@ -0,0 +1,21 @@
+
+
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+    *returnSize = numRows;
+    *returnColumnSizes = (int*)malloc(numRows * sizeof(int));
+        int** result = (int**)malloc(numRows * sizeof(int*));
+        for (int i = 0; i < numRows; i++) {
+        (*returnColumnSizes)[i] = i + 1;
+            result[i] = (int*)malloc((i + 1) * sizeof(int));
+        
+        result[i][0] = 1;
+        result[i][i] = 1;
+        
+
+        for (int j = 1; j < i; j++) {
+            result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+        }
+    }
+    
+    return result;
+}
