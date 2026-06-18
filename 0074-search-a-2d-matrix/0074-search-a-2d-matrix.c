3de82edb68e20c4bf8f23028221f73977e034ea7
Binh Truong Phuoc <binh.truongphuoc2501@hcmut.edu.vn>
2026-06-18 15:29:32 +0700

    Time: 0 ms (100%), Space: 9.7 MB (14.51%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.c
@@ -0,0 +1,31 @@
+
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    if (matrixSize == 0 || matrixColSize[0] == 0) {
+        return false;
+    }
+
+    int m = matrixSize;
+    int n = matrixColSize[0];
+
+    int left = 0;
+    int right = m * n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+
+        int mid_val = matrix[mid / n][mid % n];
+
+        if (mid_val == target) {
+            return true;
+        } 
+        else if (mid_val < target) {
+            left = mid + 1;
+        } 
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+}
